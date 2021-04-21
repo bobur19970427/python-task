eb59aa25c8d326b89a6ddef1c5c79365edd0a945
Bobur <77155984+bobur19970427@users.noreply.github.com>
2021-04-21 09:12:30 +0500

    Darslardan bajargan vazifam
    Bu dastur siz o'ylagan sonni taxmin qiladi

---
+++ geekBrains_6_taxmin_qilish.py
@@ -0,0 +1,16 @@
+import random
+son = int(input())
+start = 1
+end = 100
+belgi = None
+while belgi != "=":
+	n = random.randint(start , end)
+	print(n)
+	belgi = input()
+	if belgi == '>':
+		start = n+1
+	elif belgi == '<':
+		end = n-1
+	
+print('pobeda', n)
+
